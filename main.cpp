427c1028ed27a4c608a56d79967b5bf4c7431334
JauniKappauni <social.jauni@gmail.com>
2025-11-24 09:35:40 +0100

    feat: Implement task management functions and update main menu

--- main.cpp
+++ main.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
 using namespace std;
 
+string tasks[8];
+
+void add(){
+
+}
+
+void remove(){
+
+}
+
+void listTasks(){
+
+}
+
 int main()
 {
+    cout << "Please select a action\n";
+    cout << "1: Add a task\n";
+    cout << "2: Remove a task\n";
+    cout << "3: List all tasks\n";
+
+    int operation;
+    cin >> operation;
+
+
+    switch (operation)
+    {
+    case 1:
+    cout << "Added a task";
+        break;
+
+    case 2:
+    cout << "Removed a task";
+        break;
+
+    case 3:
+    cout << "List of all tasks";
+        break;
+    
+    default:
+        break;
+    }
 }
